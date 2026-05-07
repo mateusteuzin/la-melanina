86385f40596b7000e6bf357de716f9ba1210e7fb
mateusteuzin <106111034+mateusteuzin@users.noreply.github.com>
2026-05-07 15:51:47 -0300

    fix: Tailwind v4 CSS build

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/",
   build: {
     outDir: "dist",
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwind()],
 });
+
+
 
 
