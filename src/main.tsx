46e5338966ff0b4183268df47887d3df79971dc9
mateusteuzin <106111034+mateusteuzin@users.noreply.github.com>
2026-05-07 15:42:21 -0300

    fix: include global styles for Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -13,5 +15,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </QueryClientProvider>
   </React.StrictMode>,
 );
+
 
 
